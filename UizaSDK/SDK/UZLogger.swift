c6575f79bf88b18af932dd8fff183cf859e14b55
Nam Kennic <namkennic@me.com>
2018-05-08 11:11:19 +0700

    no message

--- UizaSDK/SDK/UZLogger.swift
+++ UizaSDK/SDK/UZLogger.swift
@@ -7,10 +7,70 @@
 //
 
 import UIKit
+import AFDateHelper
 
 /**
 Class hỗ trợ việc logging
 */
 open class UZLogger: UZAPIConnector {
+	
+	override internal func baseAPIURLPath(enviroment: UZEnviroment) -> String! {
+		return UizaSDK.apiEndPoint.stringByAppendingPathComponent("api/resource/")
+		
+		/*
+		switch enviroment {
+		case .production:
+		return "\(UizaSDK.apiEndPoint)/api/data/v1/"
+		case .development:
+		return "\(UizaSDK.apiEndPoint)/api/data/v1/"
+		case .staging:
+		return "\(UizaSDK.apiEndPoint)/api/data/v1/"
+		}
+		*/
+	}
+	
+	open func log(event: String, params: [String: Any]? = nil, completionBlock: APIConnectorResultBlock? = nil) {
+		let modelId		: String = UIDevice.current.hardwareModel()
+		let modelName	: String = UIDevice.current.hardwareName()
+		let macAddress	: String = UIDevice.current.identifierForVendor?.uuidString ?? ""
+		let appVersion	: String = UIApplication.shared.applicationVersion()
+		let userId		: String = UZUser.currentUser?.id ?? ""
+		let bundleId	: String = Bundle.main.bundleIdentifier ?? ""
+		let iosVersion	: String = UIDevice.current.systemVersion
+		let enviroment	: String = UizaSDK.enviroment.rawValue
+		let timestamp	: String = Date().toString(format: .isoDateTimeMilliSec) // Date().toString(format: .custom("yyyy-MM-dd'T'HH:mm:ss.SSSZ")) // 2018-03-15T14:19:04.637Z
+		#if os(macOS)
+		let platform	: String = "macOS"
+		#else
+		let platform	: String = UIDevice.isTV() ? "tvos" : "ios"
+		#endif
+		
+		print("timestamp: \(timestamp)")
+		let defaultParams : [String : Any]! = ["event_type" 		: event,
+											   "timestamp"			: timestamp,
+											   "platform"			: platform,
+											   "modelId"			: modelId,
+											   "modelName"			: modelName,
+											   "macAddress"			: macAddress,
+											   "version"			: appVersion,
+											   "iosVersion"			: iosVersion,
+											   "uuid"				: macAddress,
+											   "viewer_user_id"		: userId,
+											   "ip"					: UZAPIConnector.ipAddress,
+											   "player_name"		: "UizaSDK_\(platform)",
+											   "player_version" 	: PLAYER_VERSION,
+											   "sdk_version"		: SDK_VERSION,
+											   "user_agent"			: UizaSDK.appId,
+											   "bundleId"			: bundleId,
+											   "env"				: enviroment]
+		
+		var finalParams : [String: Any]! = defaultParams
+		
+		if params != nil {
+			finalParams.appendFrom(params!)
+		}
+		
+		self.callAPI("analytic-tracking", method: .post, params: finalParams, completion: completionBlock)
+	}
 
 }

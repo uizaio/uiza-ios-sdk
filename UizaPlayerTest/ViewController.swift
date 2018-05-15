1f9e515ca044e20c54f621262de24fd6c6e2c117
Nam Kennic <namkennic@me.com>
2018-05-15 13:14:55 +0700

    no message

--- UizaPlayerTest/ViewController.swift
+++ UizaPlayerTest/ViewController.swift
@@ -10,17 +10,18 @@ import UIKit
 //import UizaSDK
 
 class ViewController: UIViewController {
-	let player = UZPlayer()
+	let playerViewController = UZPlayerViewController()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		self.view.addSubview(player)
+		self.view.addSubview(playerViewController.view)
 		
 		UZAccountServices().authorize { [weak self] (token, error) in
 //			let videoItem = UZVideoItem(data: ["id" : "5f1b718b-157e-459b-ae42-1915991e9f72", "title" : "La Vie En Rose"])
-			let videoItem = UZVideoItem(data: ["id" : "290cbb8e-98a3-4568-b9ca-3761c6e0f91d", "title" : "120309_SBS_ALIVE_BIGBANG"])
-			self?.player.loadVideo(videoItem)
+//			let videoItem = UZVideoItem(data: ["id" : "290cbb8e-98a3-4568-b9ca-3761c6e0f91d", "title" : "120309_SBS_ALIVE_BIGBANG"])
+			let videoItem = UZVideoItem(data: ["id" : "27986b40-75ff-4776-b279-175d8ee50257", "title" : "Big_Buck_Bunny_1080p_LANG_ENG"])
+			self?.playerViewController.player.loadVideo(videoItem)
 		}
 	}
 	
@@ -29,7 +30,7 @@ class ViewController: UIViewController {
 		
 		let viewSize = self.view.bounds.size
 		let playerSize = CGSize(width: viewSize.width, height: viewSize.width * (3/4))
-		player.frame = CGRect(x: 0, y: (viewSize.height - playerSize.height)/2, width: playerSize.width, height: playerSize.height)
+		playerViewController.view.frame = CGRect(x: 0, y: (viewSize.height - playerSize.height)/2, width: playerSize.width, height: playerSize.height)
 	}
 
 }
--- UizaSDK/Player/UZPlayerViewController.swift
+++ UizaSDK/Player/UZPlayerViewController.swift
@@ -7,50 +7,121 @@
 //
 
 import UIKit
+import NKModalViewManager
 
 open class UZPlayerViewController: UIViewController {
-	public let player = UZPlayer()
+	internal let playerController = UZPlayerController()
+	lazy open var player: UZPlayer = {
+		return playerController.player
+	}()
 	
-	override open func loadView() {
-		self.view = self.player
-//		NotificationCenter.default.addObserver(self, selector: #selector(onDeviceRotated), name: .UIApplicationDidChangeStatusBarOrientation, object: nil)
+	var autoFullscreenWhenRotateDevice = true
+	
+	var isFullscreen: Bool {
+		get {
+			return NKModalViewManager.sharedInstance().modalViewControllerThatContains(playerController) != nil
+		}
+		set {
+			if newValue {
+				if !isFullscreen {
+					NKModalViewManager.sharedInstance().presentModalViewController(self.playerController)
+				}
+			}
+			else if let modalViewController = NKModalViewManager.sharedInstance().modalViewControllerThatContains(playerController) {
+				modalViewController.dismissWith(animated: true, completion: {
+					self.viewDidLayoutSubviews()
+				})
+			}
+		}
 	}
 	
-	// MARK: -
-	
-	/*
-	@objc func onDeviceRotated() {
-//		let currentOrientation = UIApplication.shared.statusBarOrientation;
-//		DLog("OK \(currentOrientation.rawValue)")
+	override open func viewDidLoad() {
+		super.viewDidLoad()
 		
-//		if UIInterfaceOrientationIsPortrait(currentOrientation) {
-//			if let fullscreenViewController = FullscreenManager.sharedInstance().fullscreenViewControllerThatContains(self) {
-//				fullscreenViewController.dismissView(animated: true, completion: nil)
-//			}
-//		}
-//		else if (UIInterfaceOrientationIsLandscape(currentOrientation)) {
-//			if FullscreenManager.sharedInstance().fullscreenViewControllerThatContains(self) == nil {
-//				FullscreenManager.sharedInstance().presentFullscreenViewController(self).enableDragToDismiss = false
-//			}
-//		}
+		playerController.player.fullscreenBlock = { [weak self] (fullscreen) in
+			guard let `self` = self else { return }
+			
+			if let modalViewController = NKModalViewManager.sharedInstance().modalViewControllerThatContains(self.playerController) {
+				modalViewController.dismissWith(animated: true, completion: {
+					self.viewDidLayoutSubviews()
+				})
+			}
+			else {
+				NKModalViewManager.sharedInstance().presentModalViewController(self.playerController)
+			}
+		}
+		
+		self.view.addSubview(self.player)
+		NotificationCenter.default.addObserver(self, selector: #selector(onDeviceRotated), name: .UIApplicationDidChangeStatusBarOrientation, object: nil)
 	}
-	*/
+	
+	override open func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		
+		if playerController.player.superview == self.view {
+			playerController.player.frame = self.view.bounds
+		}
+	}
 	
 	// MARK: -
 	
-//	override var prefersStatusBarHidden: Bool {
-//		return true
-//	}
+	
+	@objc func onDeviceRotated() {
+		if autoFullscreenWhenRotateDevice {
+			let currentOrientation = UIApplication.shared.statusBarOrientation;
+			self.isFullscreen = UIInterfaceOrientationIsLandscape(currentOrientation)
+		}
+	}
+	
+	// MARK: -
+	
+	override open var prefersStatusBarHidden: Bool {
+		return true
+	}
 	
 	override open var shouldAutorotate : Bool {
 		return true
 	}
 	
 	override open var supportedInterfaceOrientations : UIInterfaceOrientationMask {
-		return UI_USER_INTERFACE_IDIOM() == .phone ? .landscape : .all
+		return .all
 	}
 	
 	override open var preferredInterfaceOrientationForPresentation : UIInterfaceOrientation {
+		let currentOrientation = UIApplication.shared.statusBarOrientation
+		
+		if UI_USER_INTERFACE_IDIOM() == .phone {
+			return UIInterfaceOrientationIsLandscape(currentOrientation) ? currentOrientation : .landscapeRight
+		}
+		else {
+			return currentOrientation
+		}
+	}
+	
+}
+
+internal class UZPlayerController: UIViewController {
+	internal let player = UZPlayer()
+	
+	override func loadView() {
+		self.view = player
+	}
+	
+	// MARK: -
+	
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+	
+	override var shouldAutorotate : Bool {
+		return true
+	}
+	
+	override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+		return .all
+	}
+	
+	override var preferredInterfaceOrientationForPresentation : UIInterfaceOrientation {
 		let currentOrientation = UIApplication.shared.statusBarOrientation
 		
 		if UI_USER_INTERFACE_IDIOM() == .phone {

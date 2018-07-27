5d69fd24eb7196d2e8a16c65ca831888061bf754
Nam Kennic <namkennic@me.com>
2018-07-28 00:44:11 +0700

    no message

--- UizaSDK/SDK/UizaSDK.swift
+++ UizaSDK/SDK/UizaSDK.swift
@@ -17,8 +17,8 @@ import UIKit
 // pod trunk push UizaSDK.podspec
 //
 
-internal let SDK_VERSION = "3.2.1"
-internal let PLAYER_VERSION = "2.0"
+internal let SDK_VERSION = "3.2.2"
+internal let PLAYER_VERSION = "2.1"
 
 /**
 Class khởi tạo SDK
